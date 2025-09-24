5d90ec8377453236b7158bbedb122bf3d52077e3
Thomas VINET <thomas.vinet3@gmail.com>
2025-09-24 23:29:54 +0200

    alacritty base config + lsp setup

--- nvim/lua/core/plugins/mason.lua
+++ nvim/lua/core/plugins/mason.lua
@@ -1,4 +1,13 @@
 return {
     "mason-org/mason.nvim",
-    opts = {}
+    dependencies = {
+        "mason-org/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig"
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "vue_ls" }
+        })
+    end
 }
