f3b029927005be587900c114a2adfb81b59d14b4
Thomas VINET <thomas.vinet@inria.fr>
2025-10-21 11:25:20 +0200

    terminal keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 local keymap = vim.keymap.set
 
 keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Effacer le surlignage de la recherche" })
+keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = "Quit terminal mode" })
