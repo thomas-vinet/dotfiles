7fb82b9c2c9e0809b98b4e24acdc560dff573719
Thomas VINET <thomas.vinet3@gmail.com>
2025-10-22 21:29:41 +0200

    quickfix

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,3 +3,12 @@ vim.g.mapleader = " "
 local keymap = vim.keymap.set
 
 keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Effacer le surlignage de la recherche" })
+
+local function quickfix()
+  vim.lsp.buf.code_action({
+    filter = function(a) return a.isPreferred end,
+    apply = true
+  })
+end
+
+vim.keymap.set('n', '<leader>qf', quickfix, { noremap = true, silent = true, desc = "Apply quickfix" })
